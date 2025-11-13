2fa2063806b3822b9d82ba285f71467b2a6678e8
Celestrav01 <divinerohitkr@gmail.com>
2025-11-13 12:42:53 +0530

    Final Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,22 @@ const App = () => {
   return (
     <div className="min-h-screen bg-vyom-charcoal font-sans">
       
-      {/* 0. Navbar Section (Place before Hero) */}
+      
       <Navbar /> 
       
-      {/* 1. Hero Section */}
+     
       <Hero />
       
-      {/* 2. About Section */}
+      
       <About />
       
-      {/* 3. Capabilities / Products Section */}
+     
       <Capabilities />
       
-      {/* 4. Highlights Section */}
+      
       <Highlights />
       
-      {/* 5. Contact / Footer Section */}
+      
       <Footer />
       
     </div>
